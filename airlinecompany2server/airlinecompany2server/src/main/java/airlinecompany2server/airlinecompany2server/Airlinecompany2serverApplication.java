df8fdb823b0158bd217ddd2b128f61a2225fcece
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-23 11:21:49 +0200

    init: Initialized Spring project.

---
+++ airlinecompany2server/airlinecompany2server/src/main/java/airlinecompany2server/airlinecompany2server/Airlinecompany2serverApplication.java
@@ -0,0 +1,13 @@
+package airlinecompany2server.airlinecompany2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airlinecompany2serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Airlinecompany2serverApplication.class, args);
+	}
+
+}
---
+++ airlinecompany2server/airlinecompany2server/src/test/java/airlinecompany2server/airlinecompany2server/Airlinecompany2serverApplicationTests.java
@@ -0,0 +1,13 @@
+package airlinecompany2server.airlinecompany2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Airlinecompany2serverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
