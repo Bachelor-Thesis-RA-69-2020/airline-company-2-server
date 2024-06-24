9c5d583a8a0bcf951b36c8401e6d912989704a7b
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2024-06-24 11:23:21 +0200

    feat: Configured the app to use SOAP protocol and generate a WSDL file.

---
+++ airlinecompany2server/airlinecompany2server/src/main/java/airlinecompany2server/airlinecompany2server/configuration/SoapConfiguration.java
@@ -0,0 +1,40 @@
+package airlinecompany2server.airlinecompany2server.configuration;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class SoapConfiguration {
+
+    @Bean
+	public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
+		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+		messageDispatcherServlet.setApplicationContext(context);
+		messageDispatcherServlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean<MessageDispatcherServlet>(messageDispatcherServlet, "/ws/*");
+	}
+
+    @Bean(name = "airline-company-3")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema airlineCompanySchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("AirlineCompany3Port");
+        wsdl11Definition.setTargetNamespace("http://localhost:8082/");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setSchema(airlineCompanySchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema airlineCompanySchema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/airline-company-3.xsd"));
+    }
+}
